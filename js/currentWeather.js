eccd98e498cc766d79486ded1ea9e240dd655a24
koyaaoyama <aoyama.ko0920@gmail.com>
2023-08-09 16:22:13 -0700

    Implement DOM control of current weather section

--- js/currentWeather.js
+++ js/currentWeather.js
@@ -68,6 +68,14 @@ const getCurrentWeatherEndpoint = (apiKey, lat = 49.2827, lon = -123.1207, units
 }
 
 /**
+ * Returns an URL of a weather icon
+ *
+ * @param {string} iconId
+ * @returns {string}
+ */
+const getIconUrl = (iconId) => `https://openweathermap.org/img/wn/${iconId}@2x.png`
+
+/**
  * Call current weather API
  *
  * @param {string} apiKey
@@ -86,20 +94,79 @@ const getCurrentWeather = async (apiKey, lat, lon, units) => {
   }
 }
 
-// TODO: create ui by the data
-const generateUI = async () => {
-  // TODO: call api
-  // TODO: create elements and return
+const getCurrentWeatherDev = async () => dummy
 
-  const res = await getCurrentWeather(OPEN_WEATHER_API_KEY)
-  console.log(res)
+/**
+ * Generate UI by the provided data
+ *
+ * @param {string} cityName
+ * @param {string} iconId
+ * @param {number} temp
+ * @param {string} weather
+ * @param {string} description
+ * @param {number} feelsLike
+ * @param {number} max
+ * @param {number} min
+ * @param {number} humidity
+ * @param {number} pressure
+ */
+const generateUI = (
+  cityName,
+  iconId,
+  temp,
+  weather,
+  description,
+  feelsLike,
+  max,
+  min,
+  humidity,
+  pressure,
+) => {
+  const idTextMap = {
+    'current-weather-city-name': cityName,
+    'current-weather-temp': `${Math.round(temp)}°C`,
+    'current-weather-main': weather,
+    'current-weather-description': description,
+    'current-weather-feels-like': `Feels like: ${feelsLike}°C`,
+    'current-weather-temp-max': `Max: ${Math.round(max)}°C`,
+    'current-weather-temp-min': `Min: ${Math.round(min)}°C`,
+    'current-weather-humidity': `Humidity: ${humidity}%`,
+    'current-weather-pressure': `Pressure: ${pressure}hPa`,
+  }
+
+  const assignInnerTextById = (id, text) => {
+    document.getElementById(id).innerText = text
+  }
+
+  document.getElementById('current-weather-icon').src = getIconUrl(iconId)
+
+  for (const [id, val] of Object.entries(idTextMap)) {
+    assignInnerTextById(id, val)
+  }
 }
 
 const main = async () => {
-  console.log('main func in CurrentWeather.js')
-  // TODO: get current weather
-  // TODO: generateUI()
+
+  // const res = await getCurrentWeather(OPEN_WEATHER_API_KEY)
+
+  const res = await getCurrentWeatherDev()
+  generateUI(
+    res.name,
+    res.weather[0].icon,
+    res.main.temp,
+    res.weather[0].main,
+    res.weather[0].description,
+    res.main.feels_like,
+    res.main.temp_max,
+    res.main.temp_min,
+    res.main.humidity,
+    res.main.pressure,
+  )
 }
 
 // main()
-console.log(dummy)
+// console.log(dummy)
+
+document.addEventListener('DOMContentLoaded', () => {
+  main();
+})
