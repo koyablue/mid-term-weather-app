3a76b3b9287f9bbeefc8e1b439470c61b8d2040e
たいせい <119865966+Taisei-Yamaguchi@users.noreply.github.com>
2023-08-09 22:52:31 -0700

    add icons, orgnize layout,

--- taisei/js/3hours.js
+++ taisei/js/3hours.js
@@ -1,48 +1,48 @@
 // // api= "710bf472e4db9b3a0bfc6b8984516cc1"
 
-
-// カードをクリックした際の処理
+// when card clicked, this process will happen↓
 const cardsContainer = document.querySelector('.forecast-cards');
 cardsContainer.addEventListener('click', async (event) => {
   const clickedCard = event.target.closest('.card');
-  //ここをここを自由に変える
-  const cityName="Vancouver";
+
+  // get cityName
+  const cityName = getQueryParam('city')
+  
   if (clickedCard) {
     const selectedDate = clickedCard.getAttribute('data-date');
     await fetchAndDisplay3HoursForecast(selectedDate,cityName);
   }
 });
 
-// 3時間ごとの情報を取得し表示する関数
+
+//1. 3hours func
 async function fetchAndDisplay3HoursForecast(selectedDate,cityName) {
-  // OpenWeatherMap API キーを指定
   const apiKey = '710bf472e4db9b3a0bfc6b8984516cc1';
-  // OpenWeatherMap API からデータを取得
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&cnt=40&units=metric`;
   const response = await fetch(url);
   const data = await response.json();
 
-  // 選択された日付に該当する情報を抽出
+  // extract info related to selectedDate
   const selectedDateData = data.list.filter(item => {
     const timestamp = new Date(item.dt * 1000);
-    const localTime = formatLocalTime(timestamp); // ローカル時間に変換してフォーマット
+    const localTime = formatLocalTime(timestamp);
     const date = timestamp.toISOString().split('T')[0];
     return date === selectedDate;
   });
 
-  // HTMLに表示するための処理
+  // edit HTML here
   const forecastContainer = document.querySelector('.forecasts-3hours');
   forecastContainer.innerHTML = '';
 
   selectedDateData.forEach(item => {
     const timestamp = new Date(item.dt * 1000);
-    const localTime = formatLocalTime(timestamp); // ローカル時間に変換してフォーマット
+    const localTime = formatLocalTime(timestamp); 
     const weatherDescription = item.weather[0].description;
     const pop = Math.floor(item.pop*100);
     const temp = item.main.temp;
     const humidity = item.main.humidity;
 
-    // 天気コードに対応する画像を取得
+    // get weather icon
     const weatherIconCode = getWeatherIconCode(weatherDescription);
     const weatherIconUrl = getWeatherIconUrl(weatherIconCode);
 
@@ -50,7 +50,6 @@ async function fetchAndDisplay3HoursForecast(selectedDate,cityName) {
     forecastItem.classList.add('forecast-item');
     forecastItem.innerHTML = `
       <h2 class="time">${localTime}</h2>
-      <p class="weather">${weatherDescription}</p>
       <img src="${weatherIconUrl}" alt="${weatherDescription}">
       <p class="pop">Pop: ${pop}%</p>
       <p class="temp">Temp: ${temp.toFixed(2)}°C</p>
@@ -61,30 +60,24 @@ async function fetchAndDisplay3HoursForecast(selectedDate,cityName) {
   });
 }
 
-// ローカル時間を0:00 ~ 3:00 のフォーマットに変換する関数
+
+
+
+
+//2. change localTime format func
 function formatLocalTime(timestamp) {
   const localOffset = 0; 
   const localDate = new Date(timestamp.getTime() + localOffset * 60 * 60 * 1000);
-
   const startHour = Math.floor(localDate.getHours() / 3) * 3;
   const formattedStartHour = startHour === 0 ? '0:00' : `${startHour}:00`;
-  const formattedEndHour = `${startHour + 3}:00`;
-
-  const formattedTime = `${formattedStartHour} ~ ${formattedEndHour}`;
-
+  // const formattedEndHour = `${startHour + 3}:00`;
+  const formattedTime = `${formattedStartHour} ~ `;
   return formattedTime;
 }
 
 
-
+//3. get weather ico code
 function getWeatherIconCode(weatherDescription) {
-  // 天気説明を基に天気コードを取得
-  // 例: 'light rain' -> '09d'
-  // 注意: 実際のデータと一致させる必要があります
-  // マッピングにない場合は適切な対応を行ってください
-  // （空文字列やデフォルトのコードなど）
-  // この例では手動でマッピングを作成しています
-  // 実際にはAPIの仕様やデータに合わせて適切に取得する必要があります
   const weatherIconMapping = {
     'clear sky': '01d',
     'clouds': '02d',
@@ -94,6 +87,7 @@ function getWeatherIconCode(weatherDescription) {
     'overcast clouds':'04d',
     'light rain': '09d',
     'rain': '10d',
+    'moderate rain': '09d',
     'thunderstorm': '11d',
     'snow':'13d',
     'mist':'50d',
@@ -101,8 +95,15 @@ function getWeatherIconCode(weatherDescription) {
   return weatherIconMapping[weatherDescription] || '';
 }
 
+//4. get weather icon url
 function getWeatherIconUrl(iconCode) {
-  // 天気コードに基づいてアイコンのURLを構築
-  // 例: '09d' -> 'http://openweathermap.org/img/w/09d.png'
   return `http://openweathermap.org/img/w/${iconCode}.png`;
 }
+
+
+
+// 5. get city parmeter from url link func
+function getQueryParam(parameter) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(parameter);
+};
--- taisei/js/5days.js
+++ taisei/js/5days.js
@@ -1,5 +1,5 @@
 // api= "710bf472e4db9b3a0bfc6b8984516cc1"
-//5days
+//1. 5days
 async function fetch5DaysForecast(cityName) {
   const apiKey = '710bf472e4db9b3a0bfc6b8984516cc1';
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}&cnt=40&units=metric`;
@@ -20,7 +20,7 @@ async function fetch5DaysForecast(cityName) {
 
 
 
-//function for editing raw data
+//2. function for editing raw data
 function aggregate5DaysData(data) {
   const aggregatedData = {};
 
@@ -68,6 +68,7 @@ function aggregate5DaysData(data) {
   return aggregatedData;
 }
 
+//3. calculate average func
 function calculateAverage(arr) {
   const sum = arr.reduce((total, value) => total + value, 0);
   return sum / arr.length;
@@ -75,7 +76,7 @@ function calculateAverage(arr) {
 
 
 
-//get common weather 
+//4. get common weather 
 function getMostCommonWeather(weatherDescriptions) {
     const weatherCounts = {};
     
@@ -104,29 +105,32 @@ function getMostCommonWeather(weatherDescriptions) {
 }
 
 
-//edit dom
-// function displayForecastDataInTable(data) {
-//     const tableBody = document.querySelector('#fiveDaysForecastTable tbody');
-//     tableBody.innerHTML = '';
-  
-//     for (const date in data) {
+//5. get weather ico code
+function getWeatherIconCode(weatherDescription) {
+  const weatherIconMapping = {
+    'clear sky': '01d',
+    'few clouds': '02d',
+    'scattered clouds': '03d',
+    'broken clouds': '04d',
+    'light rain': '10d',
+    'rain': '10d',
+    'moderate rain': '10d',
+    'shower rain':'09d',
+    'heavy intensity rain':'10d',
+    'thunderstorm': '11d',
+    'snow':'13d',
+    'mist':'50d',
+  };
+  return weatherIconMapping[weatherDescription] || '';
+}
+//6. get weather icon url
+function getWeatherIconUrl(iconCode) {
+  return `http://openweathermap.org/img/w/${iconCode}.png`;
+}
 
-//       if (data.hasOwnProperty(date)) {
-//         const dailyInfo = data[date];
-//         const mostCommonWeather = getMostCommonWeather(dailyInfo.weatherDescriptions);
-  
-  
-//         const row = document.createElement('tr');
-//         row.innerHTML = `
-//           <td>${date}</td>
-//           <td>${mostCommonWeather}</td>
-//           <td>${dailyInfo.avgTemp.toFixed(2)}</td>
-//         `;
-  
-//         tableBody.appendChild(row);
-//       }
-//     }
-//   }
+
+
+//7. edit HTML func
 function displayForecastDataInCards(data) {
   const cardsContainer = document.querySelector('.forecast-cards');
   cardsContainer.innerHTML = '';
@@ -136,7 +140,7 @@ function displayForecastDataInCards(data) {
       const dailyInfo = data[date];
       const mostCommonWeather = getMostCommonWeather(dailyInfo.weatherDescriptions);
 
-      // 天気コードに対応する画像を取得
+      // get weather icon 
       const weatherIconCode = getWeatherIconCode(mostCommonWeather);
       const weatherIconUrl = getWeatherIconUrl(weatherIconCode);
 
@@ -147,10 +151,9 @@ function displayForecastDataInCards(data) {
       card.innerHTML = `
         <div class="card" data-date="${formattedDate}" id="card-button">
           <h2><strong>${date}</strong></h2>
-          <p>${mostCommonWeather}</p>
           <img src="${weatherIconUrl}" alt="${mostCommonWeather}">
         
-          <p>Temp: ${dailyInfo.avgTemp.toFixed(2)} °C</p>
+          <p>${dailyInfo.avgTemp.toFixed(2)} °C</p>
         </div>
       `;
 
@@ -158,43 +161,10 @@ function displayForecastDataInCards(data) {
     }
   }
 }
-  
 
 
-
-function getWeatherIconCode(weatherDescription) {
-  // 天気説明を基に天気コードを取得
-  // 例: 'light rain' -> '09d'
-  // 注意: 実際のデータと一致させる必要があります
-  // マッピングにない場合は適切な対応を行ってください
-  // （空文字列やデフォルトのコードなど）
-  // この例では手動でマッピングを作成しています
-  // 実際にはAPIの仕様やデータに合わせて適切に取得する必要があります
-  const weatherIconMapping = {
-    'clear sky': '01d',
-    'few clouds': '02d',
-    'scattered clouds': '03d',
-    'broken clouds': '04d',
-    'light rain': '09d',
-    'rain': '10d',
-    'thunderstorm': '11d',
-    'snow':'13d',
-    'mist':'50d',
-  };
-  return weatherIconMapping[weatherDescription] || '';
-}
-
-function getWeatherIconUrl(iconCode) {
-  // 天気コードに基づいてアイコンのURLを構築
-  // 例: '09d' -> 'http://openweathermap.org/img/w/09d.png'
-  return `http://openweathermap.org/img/w/${iconCode}.png`;
-}
-
-
-  //finally call 
-  async function main() {
-    // ↓ここを自由に変える
-      const cityName = 'Vancouver';
+  //8. finally call func
+  async function main(cityName) {
     
       try {
         const fiveDaysData = await fetch5DaysForecast(cityName);
@@ -203,26 +173,18 @@ function getWeatherIconUrl(iconCode) {
         console.error('Error in main:', error);
       }
   }
-  
-  main();
+
   
 
-  // const allCards = document.querySelectorAll('.forecast-card');
-  // allCards.forEach(card => {
-  //   card.addEventListener('click', (event) => {
-  //     const clickedCard = event.currentTarget;
-      
-  //     // クリックされたカードにクラスを追加
-  //     clickedCard.classList.toggle('clicked');
-      
-  //     // すでにクリックされたカード以外のクラスをリセット
-  //     allCards.forEach(otherCard => {
-  //       if (otherCard !== clickedCard) {
-  //         otherCard.classList.remove('clicked');
-  //       }
-  //     });
-    
-  //     // ここで他の部分の表示を切り替える処理を実装することができます
-  //   });
-  // });
+// 9. get city parmeter from url link func
+function getQueryParam(parameter) {
+  const urlParams = new URLSearchParams(window.location.search);
+  return urlParams.get(parameter);
+}
+
+// get cityName
+const cityName = getQueryParam('city');
+//call main()
+main(cityName);
   
+
